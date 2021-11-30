9895b5a056c8fc1df2880e3e8b899fb9ffeb167a
RobertaMatera <materaroberta11@gmail.com>
2021-11-30 17:40:13 +0100

    card displayed

--- src/index.js
+++ src/index.js
@@ -6,23 +6,23 @@ const loadAlbumCards = () => {
 
 .then(response => response.json())
 
-.then(data => {
-    console.log(data)
+.then(music => {
+    console.log(music)
     
     const albumsRow = document.querySelector('#new-section .row')
     
-    for (let i = 0; i < data.length; i++) {
-        let dataElement = data[i]
+    for (let i = 0; i < music.data.length; i++) {
+        let musicAlbum = music.data[i]
         let col = document.createElement('div')
-        col.className = 'col-3'
+        col.className = 'column'
         col.innerHTML = 
-        `<div class="card">
-            <img src=${dataElement.album.cover} class="card-img-top" alt=${dataElement.album.title}>
-            <div class="card-body">
-                <h5 class="card-title">${dataElement.album.title}</h5>
-                <p class="card-text">${dataElement.artist.name}</p>
-                <p class="card-text">${dataElement.duration}</p>
-                <a href="${dataElement.artist.link}" class="btn btn-primary">View more</a>
+        `<div class="card p-2">
+            <img src=${musicAlbum.album.cover} class="card-img-top" alt=${musicAlbum.album.title}>
+            <div class="card-body p-0 my-1">
+                <h5 class="card-title ">${musicAlbum.album.title}</h5>
+                <p class="card-text">${musicAlbum.artist.name}</p>
+                <p class="card-text">${musicAlbum.duration}</p>
+                <a href="${musicAlbum.artist.link}" class="btn btn-primary">View more</a>
             </div>
         </div>`
         
