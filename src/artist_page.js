4469edf17a488e43f985e4bad775352f45682a43
RobertaMatera <materaroberta11@gmail.com>
2021-11-30 22:44:42 +0100

    dinamically create new section on artist page

--- src/artist_page.js
+++ src/artist_page.js
@@ -9,19 +9,20 @@ const loadAlbumCards = () => {
 .then(music => {
     console.log(music)
     
-    const albumsRow = document.querySelector('#new-section .row')
-    
-    for (let i = 0; i < music.data.length; i++) {
+    const albumsRow = document.querySelector('#new-section .row:last-child')
+
+
+    for (let i = 0; i < 6; i++) {
         let musicAlbum = music.data[i]
         let col = document.createElement('div')
-        col.className = 'column'
+        col.className = 'col-2'
         col.innerHTML = 
         `<div class="card p-2">
             <img src=${musicAlbum.album.cover} class="card-img-top" alt=${musicAlbum.album.title}>
             <div class="card-body p-0 my-1">
                 <h5 class="card-title ">${musicAlbum.album.title}</h5>
                 <p class="card-text">${musicAlbum.artist.name}</p>
-                <p class="card-text">${musicAlbum.duration}</p>
+                <p class="card-text">${musicAlbum.duration} min</p>
                 <a href="${musicAlbum.artist.link}" class="btn btn-primary">View more</a>
             </div>
         </div>`
