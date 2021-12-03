3dc6c6125c6457fa9f9057e2d49397dd8f3e0309
RobertaMatera <materaroberta11@gmail.com>
2021-12-03 11:44:56 +0100

    fectch data function

--- src/search.js
+++ src/search.js
@@ -1,10 +1,28 @@
+const fetchData = () => {
+        fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=queen", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+            "x-rapidapi-key": "9cb56f5722mshf46421dc6a74c54p121d14jsn956fd468120d"
+        }
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+
+
+
 let search = document.querySelector('#search')
 
 const createSearchBar = () => {
     const divSearch = document.createElement("div") 
     divSearch.innerHTML = 
     `<form action=" ">
-        <input type="text" placeholder="Search.." name="search">
+    <input type="text" placeholder="Search.." name="search">
     </form>`
     
     search.appendChild(divSearch)
@@ -13,4 +31,5 @@ const createSearchBar = () => {
 search.addEventListener("click", createSearchBar, {once: true})
 
 window.onload = () => {
+    fetchData()
 }  
